af880cf12fbbf33cfde5e6c07a050270e43b14a7
sprachan <sprachan@bowdoin.edu>
2023-12-01 10:39:07 -0500

    clean up through individual data frame creation

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,89 +1,78 @@
-#====== Load Packages
+#====== Load Packages ==========================================================
 library(dplyr)
 
-#====== Reading in Data =================================================
+#====== Read and Tidy Data =====================================================
 # Behavioral data (collected 2023)
-behav.data <- read.csv('./data/behav_data.csv')
+behav_data <- read.csv('./data/behav_data.csv') |>
+              mutate(band = as.character(band))
 
-historic.data <- read.csv('./data/historic_data.csv') |>
-                 select(Year, Burrow, Adult.1.ID, Adult.2.ID) #only take the relevant columns
+historic_data <- read.csv('./data/historic_data.csv') |>
+                 select(Year, Burrow, Adult.1.ID, Adult.2.ID) |>
+                mutate(Adult.2.ID = as.character(Adult.2.ID))
 
 # Read in 2022 data, only take columns I need; rename columns for consistency
-data.2022 <- read.csv('./data/petrel_data2022.csv') |>
+data_2022 <- read.csv('./data/petrel_data2022.csv') |>
              select(Band, ATY.or.Chick, Burrow) |> 
-             rename('band'= Band)
+             rename('band'= Band) |>
+             mutate(band = as.character(band))
 
 # Rename columns for consistency
-sex.data <- read.csv('./data/petrel_sex.csv') |> 
-            rename('band' = Band, 'sex' = Sex)
+sex_data <- read.csv('./data/petrel_sex.csv') |> 
+            select(-Notes) |>
+            rename('band' = Band, 'sex' = Sex) |>
+            mutate(band = as.character(band))
 
 # 2023 physical characteristic data
 phys_data <- read.csv('./data/data_2023.csv') |>
              select(Band, Burrow, Mass..g., Wing.Chord..mm., Tarsus.Length..mm.,
-                    Known.Sex, PCR.Sex,
+                    Known.Sex,
                     Blood.for.PCR., Feathers.Collected.) |>
+  # clean up names
              rename(weight = Mass..g.,
                     wing_length = Wing.Chord..mm.,
                     tarsus_length = Tarsus.Length..mm.,
                     prev_sex = Known.Sex,
-                    pcr_sex = PCR.Sex,
                     blood = Blood.for.PCR.,
                     feathers = Feathers.Collected.,
-                    band = Band)
+                    band = Band,
+                    burrow = Burrow) |>
+             mutate(feathers = case_when(feathers == 'Yes' ~ 1,
+                                         feathers == 'No' ~ 0,
+                                         feathers != 'Yes' | feathers != 'No' ~ NA))
 
-#====== Make Column names and types consistent ============================
+#===== Compile individual metadata =============================================
 
-# Make sure bands are characters, not integers!
-behav.data$band <- as.character(behav.data$band)
+# Get a vector of the unique bands in the behavioral data -- these are the
+#> individuals we captured
+bands <- unique(behav_data$band[!is.na(behav_data$band)])
 
-# ATY1 bands are already as character vectors,
-#::: so only need to make sure that Adult 2 bands are 
-#::: character vectors
-historic.data$Adult.2.ID <- as.character(historic.data$Adult.2.ID)
+# Set up a storage matrix. First column is the band, second column
+#> is the number of times the bird has appeared in the data -- that is,
+#> the years of experience the bird has from grubbing
 
-# Make sure band is a character vector in 2022 and petrel sex data
-data.2022$band <- as.character(data.2022$band)
-sex.data$band <- as.character(sex.data$band)
-
-#===== Calculate Years Experience ======================================
-##Goal: Create a data frame ("individuals") that has a row for each individual
-##...with a column that contains an integer for the years experience
-##...for each bird, where years experience is the number of times the bird
-##...has been captured and grubbed in the historic data through 2022, 
-##... not including 2020 and 2021, which had little data collected with
-##...different protocols.
-
-# Get a vector of the unique bands in the behavioral data; get rid of any NAs
-#...of which there shouldn't be any, but just in case.
-bands <- unique(behav.data$band[!is.na(behav.data$band)])
-
-# Set up a storage matrix. First column is the band, second column ...
-# ...is the sum of the times the bird appeared in historic
 individuals <- matrix(0, nrow=length(bands), ncol=2)
 individuals[,1] <- bands
-#column 2 = appearances in historic and 2022
 
 # Calculate years appeared for each individual (each band number)
 for(i in 1:length(bands)){
-  #get the current band number
   b <- bands[i]
   
-  #count the number of times current band shows up in historic data
-  h <- sum(historic.data$Adult.1.ID==b, historic.data$Adult.2.ID==b, na.rm=TRUE)
+  # count the number of times current band shows up in historic data
+  h <- sum(historic_data$Adult.1.ID==b, historic_data$Adult.2.ID==b, na.rm=TRUE)
   
-  #if a band appeared at all in 2022, set t to 1 (for 1 occurrence)
-  #... otherwise, set t to 0
+  # if a band appeared at all in 2022, set t to 1 (for 1 occurrence)
+  #> otherwise, set t to 0
   # this accounts for any bands that were (for whatever reason) repeated
-  # ...in the 2022 data
-  t <- sum(data.2022$band==b)
-  if(t >0){
+  #> in the 2022 data
+  t <- sum(data_2022$band == b)
+  if(t > 0){
     t <- 1
   }else{
-    t=0
+    t = 0
   }
   
   #occurrences is the number of historic data appearances+2022 appearances
-  #... for band i
+  #> for band i
   o <- h+t
   
   #if o is 0, check if we banded it this year (band starts with 3081-02)
@@ -93,7 +82,6 @@ for(i in 1:length(bands)){
   if(o == 0){
     if(grepl('308102', b)){
       o <- 0
-      cat(b, 'banded 2023', '   ')
     }else{
       o <- NA
     }
@@ -103,67 +91,14 @@ for(i in 1:length(bands)){
   individuals[i,2] <- o
 }
 #turn the storage matrix into a data frame
-individuals <- data.frame(band=individuals[,1], years=individuals[,2])
-#make sure the years experience is coded as an integer
-individuals$years <- as.integer(individuals$years)
+individuals <- data.frame(band = individuals[,1], 
+                          years = individuals[,2]) |>
+               mutate(years = as.integer(years)) |>
+               left_join(sex_data, by = join_by(band)) |>
+               left_join(phys_data, by = join_by(band))
 
-#======= Estimate age ====================================
-#set up a storage matrix
-store <- matrix(0, nrow=length(bands), ncol=2)
-#first column is the list of bands
-store[,1] <- bands
 
-#similar to getting years experience, but instead estimate age by taking
-#...the first year the bird appeared on Kent as year 3 and adding to that
-#...the number of years it has been since then.
-for(i in 1:length(bands)){
-  #get the current band number
-  b <- bands[i]
-  
-  #find the all appearances of the band (rows)in historic data
-  r <- which(historic.data$Adult.1.ID==b | historic.data$Adult.2.ID==b)
-  
-  #find the appearances of the band in 2022 data
-  r22 <- which(data.2022$band==b)
-  
-  #if the band appeared in historic data, get the row
-  #::: of the minimum year that appears
-  if(length(r)>0){
-    #get the minimum year value
-    m <- historic.data[which(historic.data[,1]==min(historic.data[r,1])), 1][1]
-    #get the age by counting the years between the first appearance and today and
-    #...adding 3 (because we assume the bird is at least 3 when it first breeds)
-    y <-2023-m+3
-    
-  }else if(grepl('308102', b)){
-    #if the band didn't appear in historic data, first check 
-    #...if the bird was banded in 2023. If it was, then set y to 3 (assuming
-    #...that the bird is breeding for the first time in its 3rd year)
-    y <- 3
-  }else if(length(r22)>0){
-    #otherwise, if it appeared in 2022 (but not in historic data and wasn't
-    #...newly banded in 2023), set y to 4
-    y <- 4
-  }else{
-    #otherwise, set to NA
-    y <- NA
-  }
-  store[i,2] <- y
-}
-#turn the storage matrix into a data frame
-store <- data.frame(band=store[,1], est_age=store[,2])
-
-#add this age information to the individuals data frame
-individuals <- left_join(individuals, store, by=join_by(band))
-remove(store)
-
-individuals$est_age <- as.integer(individuals$est_age)
-
-#===== Get sexes =====================================================
-#add sexes to individuals data frame
-individuals <- left_join(individuals, sex.data, by=join_by(band))
-individuals <- left_join(individuals, phys_data, by = join_by(band))
-
+ 
 
 #===== Make a by-individual behavioral data frame ====================
 #Creates a data frame where each individual has a row, and the value
