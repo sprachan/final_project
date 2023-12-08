446b6baf21458b1349e1754946d79fdc741cbac6
sprachan <sprachan@bowdoin.edu>
2023-12-08 10:42:45 -0500

    updating eda.R to remain compatible with cleaned data

--- scripts/eda.R
+++ scripts/eda.R
@@ -1,21 +1,22 @@
 library(tidyverse)
 library(corrplot)
-load('./data/behav_data_individual.RData')
+load('./data/behav_ind_raw.RData')
 
-behav.by.ind <- behav.by.ind |> mutate(run_hide = hide+run_and_hide)
+behav_ind_raw <- behav_ind_raw |> mutate(run_hide = hide+run_and_hide)
 
-behavs <- cbind(behav.by.ind$run_hide,
-                behav.by.ind$regurgitate,
-                behav.by.ind$vocalize,
-                behav.by.ind$kick,
-                behav.by.ind$bite)
+behavs <- cbind(behav_ind_raw$run_hide,
+                behav_ind_raw$regurgitate,
+                behav_ind_raw$vocalize,
+                behav_ind_raw$kick,
+                behav_ind_raw$bite)
 b <- c('run/hide', 'regurgitate', 'vocalize', 'kick', 'bite')
 behav_cor <- cor(behavs)
 colnames(behav_cor) <- b
 rownames(behav_cor) <- b
 corrplot(behav_cor, method = 'color',
          outline = TRUE, 
-         col = COL2('PRGn'))
+         col = COL2('PRGn'),
+         tl.col = 'black')
 
 # 5 chains, 50,000 iterations, get an ESS of 5000 for each parameter before running the BF
 
