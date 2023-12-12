bb93c959fe0029f5b95e9e5fd24015717e17f086
sprachan <sprachan@bowdoin.edu>
2023-12-11 21:47:13 -0500

    added confidence intervals

--- scripts/stan_m1.R
+++ scripts/stan_m1.R
@@ -8,22 +8,22 @@ make_param_df <- function(list_in){
                'regurgitate' = list_in[,3],
                'vocalize' = list_in[,4],
                'kick' = list_in[,5])
-  out <- enframe(temp, name = 'behavior', value = 'modelled_value') |>
-         unnest_longer(modelled_value)
+  out <- enframe(temp, name = 'behavior', value = 'value') |>
+         unnest_longer(value)
   return(out)
 }
 
 plot_param <- function(list_in, plot_title = NULL){
   temp <- make_param_df(list_in)
   p <- ggplot(temp)+
-       geom_histogram(aes(x = modelled_value),
+       geom_histogram(aes(x = value),
                           bins = 100,
                           fill = 'lightblue',
                           color = 'black',
                           linewidth = 0.25)+
        facet_wrap(facets = vars(behavior),
                   nrow = 5)+
-       labs(title = plot_title, x = 'Modelled Value', y = 'Count')
+       labs(title = plot_title, x = 'Value', y = 'Count')
   return(p)
 }
 
@@ -58,17 +58,43 @@ fit = sampling(model, model_obj, iter = 10000, chains = 1)
 params = rstan::extract(fit)
 
 # Plot -------------------------------------------------------------------------
-params_only <- params[which(names(params) != 'lp__')]
-plots <- imap(params_only, plot_param) 
-plots$coeff_tl <- plots$coeff_tl+xlim(c(-2, 2))
+params_only <- params[which(names(params) != 'lp__')] # coefficients only, no log prior
+plots <- imap(params_only, \(x, idx) plot_param(list_in = x, plot_title = idx))
+
+# adjust x limits so 0 is centered
+plots$coeff_tl <- plots$coeff_tl+xlim(c(-2, 2)) 
 plots$coeff_wl <- plots$coeff_wl+xlim(c(-0.4, 0.4))
 
+# Commented out for now because I have already plotted these guys
 pdf(file = './plots/m1_param_plots.pdf')
 plots
 dev.off()
 
-# Confidence Intervals on the parameters ---------------------------------------
+# Confidence Intervals on the Parameters ---------------------------------------
+# 90% CI
+ci90_df <- map(params_only, make_param_df) |>
+  map(\(x) group_by(x, behavior)) |>
+  map(\(x) summarize(x, 
+                     lower = quantile(value, 0.05),
+                     upper = quantile(value, 0.95)))
+lapply(ci90_df, print)
+# all CI's contain 0 except the WL coefficient with kick
 
+# 95% CI
+ci95_df <- map(params_only, make_param_df) |>
+           map(\(x) group_by(x, behavior)) |>
+           map(\(x) summarize(x, 
+                              lower = quantile(value, 0.025),
+                              upper = quantile(value, 0.975)))
+lapply(ci95_df, print)
+# again, all CI's contain 0 except the WL coefficient with kick 
 
+# 99% CI
+ci99_df <- map(params_only, make_param_df) |>
+           map(\(x) group_by(x, behavior)) |>
+           map(\(x) summarize(x, 
+                              lower = quantile(value, 0.005),
+                              upper = quantile(value, 0.995)))
 
-
+lapply(ci99_df, print)
+# lose significance with kick/WL coefficient 
