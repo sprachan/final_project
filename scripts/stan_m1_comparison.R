cae20d92caa4194ed40403298a4757e34981f073
Emily Simons <esimons@dhcp-223-126.student-wlan.bowdoin.edu>
2023-12-12 16:25:11 -0500

    compared all three m1 models with bayes factors

--- scripts/stan_m1_comparison.R
+++ scripts/stan_m1_comparison.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(rstan)
+library(patchwork)
+library("bridgesampling")
+library("loo")
 
 # Functions --------------------------------------------------------------------
 make_param_df <- function(list_in){
@@ -54,8 +57,26 @@ model_obj <- list(B = 5,
                   kick=kick
 )
 wt_model = stan_model('./scripts/m1_wt.stan')
-fit = sampling(wt_model, model_obj, iter = 10000, chains = 1)
-params = rstan::extract(fit)
+tl_model = stan_model('./scripts/m1_tl.stan')
+wl_model = stan_model('./scripts/m1_wl.stan')
+wt_fit = sampling(wt_model, model_obj, iter = 10000, chains = 1)
+tl_fit = sampling(tl_model, model_obj, iter = 10000, chains = 1)
+wl_fit = sampling(wl_model, model_obj, iter = 10000, chains = 1)
+params_wt = rstan::extract(wt_fit)
+params_tl = rstan::extract(tl_fit)
+params_wl = rstan::extract(wl_fit)
+
+bf.wt_wl <- bayes_factor( bridge_sampler(wt_fit, silent = TRUE),bridge_sampler(wl_fit, silent = TRUE))
+print(bf.wt_wl) #Bayes factor in favor of x1 over x2: 3.95877
+
+bf.wt_tl <- bayes_factor( bridge_sampler(wt_fit, silent = TRUE),bridge_sampler(tl_fit, silent = TRUE))
+print(bf.wt_tl) #Bayes factor in favor of x1 over x2: 0.00087 (so x2 over x1 is 1149.425)
+
+bf.tl_wl <- bayes_factor( bridge_sampler(tl_fit, silent = TRUE),bridge_sampler(wl_fit, silent = TRUE))
+print(bf.tl_wl) #Bayes factor in favor of x1 over x2: 4714.73077
+
+#so tarsus length is the best model
+
 
 # Plot -------------------------------------------------------------------------
 # params_only <- params[which(!(names(params) %in% c('lp__', 'p')))] # coefficients only, no log prior
