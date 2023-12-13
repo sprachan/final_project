5a2c4284441c28e63f185cdcc262f6088139b7bc
Emily Simons <esimons@dhcp-223-126.student-wlan.bowdoin.edu>
2023-12-13 10:30:08 -0500

    added model1 and updated bayes factors

--- scripts/stan_m1_comparison.R
+++ scripts/stan_m1_comparison.R
@@ -79,15 +79,21 @@ fit0 = sampling(model0, model_obj, iter = 10000, chains = 1)
 params0 = rstan::extract(fit0)
 
 # MODEL 1
+exp_model = stan_model('./scripts/m1_experience.stan')
 wt_model = stan_model('./scripts/m1_wt.stan')
 tl_model = stan_model('./scripts/m1_tl.stan')
 wl_model = stan_model('./scripts/m1_wl.stan')
+model1 = stan_model('./scripts/m1_comp.stan')
+exp_fit = sampling(exp_model, model_obj, iter = 10000, chains = 1)
 wt_fit = sampling(wt_model, model_obj, iter = 10000, chains = 1)
 tl_fit = sampling(tl_model, model_obj, iter = 10000, chains = 1)
 wl_fit = sampling(wl_model, model_obj, iter = 10000, chains = 1)
+fit1 = sampling(model1, model_obj, iter = 10000, chains = 1)
+params_exp = rstan::extract(exp_fit)
 params_wt = rstan::extract(wt_fit)
 params_tl = rstan::extract(tl_fit)
 params_wl = rstan::extract(wl_fit)
+params1 = rstan::extract(fit1)
 
 # MODEL 2
 model_obj2 <- model_obj
@@ -111,6 +117,9 @@ print(bf.tl_0) #Bayes factor in favor of x1 over x2: 30,000
 bf.wl_0 <- bayes_factor(bridge_sampler(wl_fit, silent = TRUE),bridge_sampler(fit0, silent = TRUE))
 print(bf.wl_0) #Bayes factor in favor of x1 over x2: 5
 
+bf.exp_0 <- bayes_factor(bridge_sampler(exp_fit, silent = TRUE),bridge_sampler(fit0, silent = TRUE))
+print(bf.exp_0) #Bayes factor in favor of x1 over x2:60,978
+
 bf.wt_wl <- bayes_factor(bridge_sampler(wt_fit, silent = TRUE),bridge_sampler(wl_fit, silent = TRUE))
 print(bf.wt_wl) #Bayes factor in favor of x1 over x2: 3.95877
 
@@ -119,6 +128,27 @@ print(bf.wt_tl) #Bayes factor in favor of x1 over x2: ~0.0009 (so x2 over x1 is 
 
 bf.tl_wl <- bayes_factor(bridge_sampler(tl_fit, silent = TRUE),bridge_sampler(wl_fit, silent = TRUE))
 print(bf.tl_wl) #Bayes factor in favor of x1 over x2: ~4000
+
+bf.exp_tl <- bayes_factor(bridge_sampler(exp_fit, silent = TRUE),bridge_sampler(tl_fit, silent = TRUE))
+print(bf.exp_tl) #Bayes factor in favor of x1 over x2: ~2
+
+bf.1_exp <- bayes_factor(bridge_sampler(fit1, silent = TRUE),bridge_sampler(exp_fit, silent = TRUE))
+print(bf.1_exp) #Bayes factor in favor of x1 over x2: .3
+
+bf.exp_1 <- bayes_factor(bridge_sampler(exp_fit, silent = TRUE),bridge_sampler(fit1, silent = TRUE))
+print(bf.exp_1) #Bayes factor in favor of x1 over x2: ~4
+
+bf.1_tl <- bayes_factor(bridge_sampler(fit1, silent = TRUE),bridge_sampler(tl_fit, silent = TRUE))
+print(bf.1_tl) #Bayes factor in favor of 1 over tarsus: 0.52
+
+bf.tl_1 <- bayes_factor(bridge_sampler(tl_fit, silent = TRUE),bridge_sampler(fit1, silent = TRUE))
+print(bf.tl_1) #Bayes factor in favor of tarsus over 1: 1.3
+
+bf.2_0 <- bayes_factor(bridge_sampler(fit2, silent = TRUE),bridge_sampler(fit0, silent = TRUE))
+print(bf.2_0) #BF in favor of model 2: 
+
+bf.2_1 <- bayes_factor(bridge_sampler(fit2, silent = TRUE),bridge_sampler(fit1, silent = TRUE))
+print(bf.2_1) #BF in favor of model 2: 
 
 bf.2_tl <- bayes_factor(bridge_sampler(fit2, silent = TRUE),bridge_sampler(tl_fit, silent = TRUE))
 print(bf.2_tl) #BF in favor of model 2: 1821627687958405896515918810588134719952546551556675007110428671458943530999606117169678155513856.00000
