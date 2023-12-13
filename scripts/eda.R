ec83b3673a41b87c1b38205b46a6114ab8c4838d
sprachan <sprachan@bowdoin.edu>
2023-12-13 11:27:27 -0500

    corrplots

--- scripts/eda.R
+++ scripts/eda.R
@@ -9,14 +9,35 @@ behavs <- cbind(behav_ind_raw$run_hide,
                 behav_ind_raw$vocalize,
                 behav_ind_raw$kick,
                 behav_ind_raw$bite)
+
+phys <- cbind(as.numeric(behav_ind_raw$weight),
+              as.numeric(behav_ind_raw$wing_length),
+              as.numeric(behav_ind_raw$tarsus_length)) |>
+        na.omit()
+
 b <- c('run/hide', 'regurgitate', 'vocalize', 'kick', 'bite')
+p <- c('weight', 'wing length', 'tarsus length')
+
 behav_cor <- cor(behavs)
 colnames(behav_cor) <- b
 rownames(behav_cor) <- b
-corrplot(behav_cor, method = 'color',
-         outline = TRUE, 
-         col = COL2('PRGn'),
-         tl.col = 'black')
+
+phys_cor <- cor(phys)
+colnames(phys_cor) <- p
+rownames(phys_cor) <- p
 
 # 5 chains, 50,000 iterations, get an ESS of 5000 for each parameter before running the BF
 
+pdf(file = './plots/corrplot_behavs.pdf')
+corrplot(behav_cor, method = 'color',
+         outline = TRUE,
+         col = COL2('PRGn'),
+         tl.col = 'black')
+dev.off()
+
+pdf(file = './plots/corrplot_phys.pdf')
+corrplot(phys_cor, method = 'color',
+         outline = TRUE,
+         col = COL2('PRGn'),
+         tl.col = 'black')
+dev.off()
